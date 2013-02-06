bc48dfac0b977239ee34f08213bea32951254198
isutanto <is17@txstate.edu>
2013-02-05 18:02:02 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
-
 public class HelloWorld {
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World ! Whaddaya wanna do t'day??");
+		System.out.println("I'm adding this new messages. Do you copy??");
 	}
 
 }
